13915b5d4515370a5de7f89e3071beaad0de0cbb
MadhanXC <91551365+MadhanXC@users.noreply.github.com>
2025-12-31 20:22:44 +0530

    Refactor firebaseConfig to use environment variables
    Updated firebaseConfig to use environment variables for sensitive information.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9682263086-db068",
-  "appId": "1:479486883490:web:330aa7d805c4af6b6bc9fb",
-  "storageBucket": "studio-9682263086-db068.appspot.com",
-  "apiKey": "AIzaSyBPndZ7_BFWdUlQVf58V9stnJ6kC6X6oRc",
-  "authDomain": "studio-9682263086-db068.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "479486883490"
+  projectId: process.env.projectId,
+  appId: process.env.appId,
+  storageBucket: process.env.storageBucket,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  messagingSenderId: process.env.messagingSenderId
 };
